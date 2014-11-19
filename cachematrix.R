dbe459e40c1e6a886df3f0d66e911b3b26887864
seanmullane <mullane.s.p@gmail.com>
2014-11-19 15:03:42 -0500

    completed assignment 2
    Filled out the 2 stub functions to complete assignment 2 for Coursera R
    Programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix and cacheSolve functions create a special matrix object
+## that can cache its inverse and allow the inverse to be retreived or, if it
+## hasn't been calculated already, to be calculated and then returned.
 
-## Write a short comment describing this function
+
+## The makeCacheMatrix function takes a square invertible matrix as its argument
+## and returns a list of functions. These functions get and set a matrix object 
+## and allow its inverse to be cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function takes a makeCacheMatrix object as its primary argument
+## and returns the inverse of the matrix contained in the object. If the object does
+## not already contain the inverse or the matrix has been modified since the inverse
+## was computed, the inverse is recalculated and returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mat <- x$get()
+  inv <- x$getinverse()
+  if(!is.null(inv)) { ## test whether cached inverse present
+    if(all.equal(mat %*% inv, diag(nrow(mat)))){ ## test whether cached inverse still valid
+      message("getting cached data")
+      return(inv)
+    }
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
